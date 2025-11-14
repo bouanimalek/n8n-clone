b9db8b25169595089c4d91080c1ce983f75268c7
bouanimalek <malzovich@gmail.com>
2025-11-14 01:02:58 +0100

    feat: better-auth initial config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,14 @@
-import { getQueryClient, trpc } from "@/trpc/server";
-import { Client } from "./client";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
+"use client";
 
-const Page = async () => {
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
 
+const Page = () => {
+  const { data } = authClient.useSession();
   return (
     <div className="min-h-screen min-w-screen flex justify-center items-center">
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <Suspense fallback={<p>Loading...</p>}>
-          <Client />
-        </Suspense>
-      </HydrationBoundary>
+      {JSON.stringify(data)}
+      {data && <Button onClick={() => authClient.signOut()}>Logout</Button>}
     </div>
   );
 };
