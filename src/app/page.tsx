2c3522b9062a940b03cf7621e2348a556f524c68
bouanimalek <malzovich@gmail.com>
2025-11-14 11:15:47 +0100

    feat: protected procedure (trpc)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
+import { Button } from "@/components/ui/button";
 import { requireAuth } from "@/lib/auth-utils";
+import { caller } from "@/trpc/server";
+import { LogoutButton } from "./logout";
 
 const Page = async () => {
   await requireAuth();
+  const data = await caller.getUsers();
   return (
-    <div className="min-h-screen min-w-screen flex justify-center items-center">
+    <div className="min-h-screen min-w-screen flex justify-center items-center flex-col gap-y-6">
       protected server component
+      <div>{JSON.stringify(data, null, 2)}</div>
+      <LogoutButton />
     </div>
   );
 };
